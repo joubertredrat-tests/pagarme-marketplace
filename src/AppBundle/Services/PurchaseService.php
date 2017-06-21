7c14221466b529463d565151b9f5640f7c512182
Joubert RedRat <me+github@redrat.com.br>
2017-06-20 22:05:18 -0300

    Create empty Purchase service

---
+++ src/AppBundle/Services/PurchaseService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Pagar.me develop exam
+ *
+ * @author Joubert RedRat <me+github@redrat.com.br>
+ * @copyright 2017, Pagar.me
+ * @link https://pagar.me
+ */
+
+namespace AppBundle\Services;
+
+/**
+ * Purchase Service
+ *
+ * @package AppBundle\Services
+ */
+class PurchaseService
+{
+
+}
