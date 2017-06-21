04a9df8b04391944e5293ca91a0b8321d3291bab
Joubert RedRat <me+github@redrat.com.br>
2017-06-20 22:10:38 -0300

    Add service dependences and create service on bundle config

--- src/AppBundle/Services/PurchaseService.php
+++ src/AppBundle/Services/PurchaseService.php
@@ -9,6 +9,10 @@
 
 namespace AppBundle\Services;
 
+use AppBundle\Repository\ProductRepository;
+use AppBundle\Repository\PurchaseProductRepository;
+use AppBundle\Repository\PurchaseRepository;
+
 /**
  * Purchase Service
  *
@@ -16,5 +20,35 @@ namespace AppBundle\Services;
  */
 class PurchaseService
 {
+    /**
+     * @var PurchaseRepository
+     */
+    private $purchaseRepository;
 
+    /**
+     * @var PurchaseProductRepository
+     */
+    private $purchaseProductRepository;
+
+    /**
+     * @var ProductRepository
+     */
+    private $productRepository;
+
+    /**
+     * PurchaseService constructor
+     *
+     * @param PurchaseRepository $purchaseRepository
+     * @param PurchaseProductRepository $purchaseProductRepository
+     * @param ProductRepository $productRepository
+     */
+    public function __construct(
+        PurchaseRepository $purchaseRepository,
+        PurchaseProductRepository $purchaseProductRepository,
+        ProductRepository $productRepository
+    ) {
+        $this->purchaseRepository = $purchaseRepository;
+        $this->purchaseProductRepository = $purchaseProductRepository;
+        $this->productRepository = $productRepository;
+    }
 }
